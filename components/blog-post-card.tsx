b106a0341e682d66a2d6f3b4a37af31f8d608057
Daniel Costa <daniel.c.dan@gmail.com>
2022-10-26 16:13:57 -0300

    Atualização

--- components/blog-post-card.tsx
+++ components/blog-post-card.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { ListRenderItem, StyleSheet } from "react-native";
-import { HStack, VStack, Icon, Box, Text, Spacer, IconButton } from "native-base";
+import { HStack, VStack, Icon, Box, Text, Spacer, IconButton, View } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 
 export const BlogPostCard: ListRenderItem<BlogPost> = ({ item: { title, body } }) =>
 <Box marginBottom={4} style={styles.postContainer}>
   <HStack space={[2, 3]} justifyContent="space-between">
-    <VStack style={styles.postText}>
+    <View style={styles.postText}>
       <Text bold>{title}</Text>
       <Spacer />
       <Text>{body}</Text>
-    </VStack>
-    <IconButton color="red" icon={<Icon as={MaterialIcons} name="delete-forever" size="sm" />} />
+    </View>
+    <IconButton icon={<Icon as={MaterialIcons} name="delete-forever" size="sm" />} />
   </HStack>
 </Box>
 
 const styles = StyleSheet.create({
-    postContainer: {
+  postContainer: {
+    flex: 1,
     borderWidth: 1,
     height: 200,
     backgroundColor: "#FFF",
     borderColor: "#FFF",
     borderRadius: 2,
   },
-    postText: {
+  postText: {
     fontSize: 16,
     fontWeight: "regular",
     padding: 20,
